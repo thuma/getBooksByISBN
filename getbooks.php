3b90cd957db3e5480efb8f9e7e45813a11136db2
Martin Thuresson <martin.thure@gmail.com>
2014-07-07 22:05:58 +0200

    Added comments and intendation.

--- getbooks.php
+++ getbooks.php
@@ -3,36 +3,50 @@
 // Check if data is posted:
 if(isset($_POST['isbns'])){
 
-// Get data to strig variable:
-$isbns = $_POST['isbns'];
+  // Get data to strig variable:
+  $isbns = $_POST['isbns'];
 
-// Split data to array:
-$isbnsa = explode("\n", $isbns);
+  // Split data to array:
+  $isbnsa = explode("\n", $isbns);
 
-// Send header with csv fild header and name, no cache:
-header("Content-type: text/csv");
-header("Content-Disposition: attachment; filename=books.csv");
-header("Pragma: no-cache");
-header("Expires: 0");
+  // Send header with csv fild header and name, no cache:
+  header("Content-type: text/csv");
+  header("Content-Disposition: attachment; filename=books.csv");
+  header("Pragma: no-cache");
+  header("Expires: 0");
 
-// Print the header for the CSV fie.
-print '"ISBN","Title","Publisher","Date","Language","KB link"';
-foreach($isbnsa as &$isbn){
-$isbn=trim($isbn);
-$book = json_decode(file_get_contents('http://libris.kb.se/xsearch?format=json&query='.$isbn));
-print "\n".'"'.$isbn.'"';
-if(isset($book->xsearch)){
-  print ',"'.$book->xsearch->list[0]->title.'"';
-  print ',"'.$book->xsearch->list[0]->publisher.'"';
-  print ',"'.$book->xsearch->list[0]->date.'"';
-  print ',"'.$book->xsearch->list[0]->language.'"';
-  print ',"'.$book->xsearch->list[0]->identifier.'"';
-  }
+  // Print the header for the CSV fie.
+  print '"ISBN","Title","Publisher","Date","Language","KB link"';
+  
+  // Loop every row.
+  foreach($isbnsa as &$isbn){
+    
+    // Remove white cars.
+    $isbn=trim($isbn);
+    
+    // Get book data form KB libirs API.
+    $book = json_decode(file_get_contents('http://libris.kb.se/xsearch?format=json&query='.$isbn));
+    
+    // Print the ISBN on new row.
+    print "\n".'"'.$isbn.'"';
+    
+    // Check if book found in libris.
+    if(isset($book->xsearch)){
+      
+      // Print all book data.
+      print ',"'.$book->xsearch->list[0]->title.'"';
+      print ',"'.$book->xsearch->list[0]->publisher.'"';
+      print ',"'.$book->xsearch->list[0]->date.'"';
+      print ',"'.$book->xsearch->list[0]->language.'"';
+      print ',"'.$book->xsearch->list[0]->identifier.'"';
+    }
   }
 }
+// If no data show simle GUI.
 else{
-header('Content-Type: text/html; charset=utf-8');
-?>
+  // Put html and utf-8 header.
+  header('Content-Type: text/html; charset=utf-8');
+  ?>
 <html>
 <body>
 <form method="POST">
